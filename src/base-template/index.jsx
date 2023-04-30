8244a3b32c16bde273a614afe86ff90df4fea0b6
Zulkar Nayin <zulkar29@gmail.com>
2023-04-30 18:41:44 +0600

    extend css stractures

--- src/base-template/index.jsx
+++ src/base-template/index.jsx
@@ -1,12 +1,23 @@
-import './index.scss';
-import Header from '../components/header';
-import Footer from '../components/footer';
-import PageRoutes from '../pages';
-import SideBar from '../components/sidebar';
+import "./index.scss";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import PageRoutes from "../pages";
+import SideBar from "../components/sidebar";
 function BaseTemplate() {
   return (
     <div>
-      <h1>Base Template</h1>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-2">
+            <SideBar />
+          </div>
+          <div className="col-md-10">
+            <PageRoutes />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,9 @@
 import './index.scss';
 function Header() {
   return (
-    <div className="header">
-      <div className="header__logo">
-        <img src={logo} alt="logo" />
-      </div>
-      <div className="header__title">React Admin</div>
-    </div>
+    <header>
+      <h3>Header item</h3>
+    </header>
   );
 }
 
