8ea95841056f790ef85313d51ac7ad42e127bb3a
Zulkar Nayin <zulkar29@gmail.com>
2023-04-30 22:30:40 +0600

    complete grid system development

--- src/base-template/index.jsx
+++ src/base-template/index.jsx
@@ -9,12 +9,16 @@ function BaseTemplate() {
       <Header />
       <div className="container">
         <div className="row">
-          <div className="col-md-2">
+         
+          <div className="col-md-3">
             <SideBar />
           </div>
-          <div className="col-md-10">
+         
+          <div className="col-md-9">
+
             <PageRoutes />
           </div>
+
         </div>
       </div>
       <Footer />
