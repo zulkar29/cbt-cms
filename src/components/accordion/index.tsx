76fd391cf055bb750ffc2e524b4e1846ed26d5e9
Zulkar Nayin <zulkar29@gmail.com>
2023-05-01 18:16:32 +0600

    work on accordion component

--- src/components/accordion/index.tsx
+++ src/components/accordion/index.tsx
@@ -1,12 +1,32 @@
-import './index.scss';
+import React, { useState } from 'react';
 
-function Accordion() {
-    return (
-        <div className="accordion">
-        <div className="accordion__title">Accordion</div>
-        <div className="accordion__content">Content</div>
+type AccordionProps = {
+  title: string;
+  children: React.ReactNode;
+}
+
+const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="accordion">
+      <div className="accordion__header" onClick={handleToggle}>
+        <h2 className="accordion__title">{title}</h2>
+        <button className="accordion__toggle">
+          {isOpen ? '-' : '+'}
+        </button>
+      </div>
+      {isOpen && (
+        <div className="accordion__body">
+          {children}
         </div>
-    );
-    }
+      )}
+    </div>
+  );
+};
 
-    export default Accordion;
+export default Accordion;
