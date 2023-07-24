bac06be969b45c76634d079c351fc76c104d385f
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-07-24 13:38:22 +0600

    setup notification popup

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,53 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { BsBell } from 'react-icons/bs';
 import './index.scss';
+import Popup from '../popup';
 
 function Header() {
-  const [isShow, setIsShow] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-  const toggleShow = () => {
-    setIsShow((prevShow) => !prevShow);
+  const handleButtonClick = () => {
+    setIsPopupOpen(true);
   };
-  useEffect(() => {
-    const handleClick = (event: MouseEvent) => {
-      const componentElement = document.getElementById('notification');
-      if (
-        componentElement &&
-        !componentElement.contains(event.target as Node)
-      ) {
-        toggleShow();
-      }
-    };
 
-    window.addEventListener('click', handleClick);
-
-    return () => {
-      window.removeEventListener('click', handleClick);
-    };
-  }, []);
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+  };
 
   return (
     <header className="header">
       <div className=" header-main">
-        <div id="notification" className="notification" onClick={toggleShow}>
+        <div
+          id="notification"
+          className="notification"
+          onClick={handleButtonClick}
+        >
           <BsBell className="notification-icon" />
           <div className="items">1</div>
         </div>
-        {isShow ? (
-          <div className="notification-items">
-            <ul>
-              <li className="notification-item">
-                <p>Notifications</p>
-              </li>
-              <li className="notification-item">
-                <p>Notifications</p>
-              </li>
-              <li className="notification-item">
-                <p>Notifications</p>
-              </li>
-            </ul>
-          </div>
-        ) : null}
+        {isPopupOpen && <Popup closePopup={handleClosePopup} />}
         <img
           className="avatar"
           src="https://geniusdevs.com/codecanyon/omnimart40/assets/images/1631023655pexels-moose-photos-1036627.jpg"
---
+++ src/components/popup/index.tsx
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect } from 'react';
+
+interface PopupProps {
+  closePopup: () => void;
+}
+
+const Popup: React.FC<PopupProps> = ({ closePopup }) => {
+  const popupRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        popupRef.current &&
+        !popupRef.current.contains(event.target as Node)
+      ) {
+        closePopup();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [closePopup]);
+
+  return (
+    <div ref={popupRef} className="notification-items">
+      <ul>
+        <li className="notification-item">
+          <p>Notifications</p>
+        </li>
+        <li className="notification-item">
+          <p>Notifications</p>
+        </li>
+        <li className="notification-item">
+          <p>Notifications</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Popup;
