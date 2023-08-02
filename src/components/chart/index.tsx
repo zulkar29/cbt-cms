12bac05e795ffb34ec70c1b6b2ef0fabc263b453
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-08-02 19:27:35 +0600

    chartbar

---
+++ src/components/chart/index.tsx
@@ -0,0 +1,43 @@
+import { BarChart, Bar, Cell, ResponsiveContainer } from 'recharts';
+
+interface IData {
+  name: string;
+  uv: number;
+  pv: number;
+  amt: number;
+}
+
+interface IProps {
+  handleClick: (entry: any, index: number) => void;
+  data: IData[];
+  activeIndex: number;
+  activeItem: IData;
+}
+const Chart: React.FC<IProps> = ({
+  handleClick,
+  data,
+  activeItem,
+  activeIndex,
+}) => {
+  return (
+    <div className="chart-card" style={{ width: '100%' }}>
+      <p>Click each rectangle </p>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart width={150} height={40} data={data}>
+          <Bar dataKey="uv" onClick={handleClick}>
+            {data.map((entry, index) => (
+              <Cell
+                cursor="pointer"
+                fill={index === activeIndex ? '#82ca9d' : '#8884d8'}
+                key={`cell-${index}`}
+              />
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+      <p className="content">{`Uv of "${activeItem.name}": ${activeItem.uv}`}</p>
+    </div>
+  );
+};
+
+export default Chart;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,83 @@
+import { useState, useCallback } from 'react';
+
 import './index.scss';
+import Chart from '../../components/chart';
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
 const HomePage = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const activeItem = data[activeIndex];
+
+  const handleClick = useCallback(
+    (entry: any, index: number) => {
+      setActiveIndex(index);
+    },
+    [setActiveIndex]
+  );
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="">
+      <div className="row">
+        <div className="chart-item col-md-6">
+          <Chart
+            data={data}
+            activeIndex={activeIndex}
+            activeItem={activeItem}
+            handleClick={handleClick}
+          />
+        </div>
+        <div className="chart-item col-md-6">
+          <Chart
+            data={data}
+            activeIndex={activeIndex}
+            activeItem={activeItem}
+            handleClick={handleClick}
+          />
+        </div>
+      </div>
     </div>
   );
 };
