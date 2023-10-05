a68803c8b744504226130d800463d9c55371565f
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-10-05 18:00:31 +0600

    video display item set

--- src/pages/videos/index.tsx
+++ src/pages/videos/index.tsx
@@ -1,3 +1,4 @@
+import { useState, ChangeEvent } from 'react';
 import Display from '../../components/display';
 import Row from '../../components/table/row';
 import Column from '../../components/table/column';
@@ -7,12 +8,20 @@ import CustomIconArea from '../../components/custom-icon-area';
 import EditButton from '../../components/button/edit';
 import DeleteButton from '../../components/button/delete';
 import ToggleButton from '../../components/forms/checkbox';
+import Filter from '../../components/filter';
 
 const VideosPage: React.FC = () => {
+  const [displayItem, setDisplayItem] = useState(10);
+
+  const handleDisplayItem = (e: ChangeEvent<HTMLSelectElement>) => {
+    setDisplayItem(Number(e.target.value));
+  };
+
   return (
     <div>
       <CardBody header="Videos" to="/videos/create" />
       <Display>
+        <Filter handleDisplayItem={handleDisplayItem} />
         <Row className="row">
           <Column className="col-md-1">SI No.</Column>
           <Column className="col-md-4">Title</Column>
@@ -23,7 +32,6 @@ const VideosPage: React.FC = () => {
         <Row className="row">
           <Column className="col-md-1">01</Column>
           <Column className="col-md-4">
-            {' '}
             Gazi Smiss Gas Stove | EG 750S | Gazi Home Appliance
           </Column>
           <Column className="col-md-4">
