52eaed7e82a1d1a2078db111d6167f673d07f00b
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-10-10 16:28:44 +0600

    review UI

--- src/pages/reviews/index.tsx
+++ src/pages/reviews/index.tsx
@@ -8,33 +8,26 @@ import Filter from '../../components/filter';
 import ToggleButton from '../../components/forms/checkbox';
 
 const Reviews: React.FC = () => {
-  const [displayItem, setDisplayItem] = useState(10);
-
-  console.log(displayItem);
-
-  const handleDisplayItem = (e: ChangeEvent<HTMLSelectElement>) => {
-    setDisplayItem(Number(e.target.value));
-  };
-
   return (
     <div>
       <CardBody header="Reviews" to="/products/reviews#" />
       <Display>
-        <Filter handleDisplayItem={handleDisplayItem} />
         <Row className="row">
           <Column className="col-md-1">#</Column>
           <Column className="col-md-2">Name</Column>
-          <Column className="col-md-3">Products</Column>
-          <Column className="col-md-4">Review</Column>
+          <Column className="col-md-2">Email</Column>
+          <Column className="col-md-2">Products</Column>
+          <Column className="col-md-3">Review</Column>
           <Column className="col-md-1">Rating</Column>
           <Column className="col-md-1">Published</Column>
         </Row>
         {[...Array(6).keys()].map((_review, index) => (
           <Row className="row" key={index}>
             <Column className="col-md-1">1</Column>
-            <Column className="col-md-2">Alex SmiColumn</Column>
-            <Column className="col-md-3">Gazi Oval Sauce Pan GSP-26C</Column>
-            <Column className="col-md-4">
+            <Column className="col-md-2">Talha</Column>
+            <Column className="col-md-2">admin@admin.com</Column>
+            <Column className="col-md-2">Gazi Oval Sauce Pan GSP-26C</Column>
+            <Column className="col-md-3">
               New French Elegant White Bubble Sleeve Party Dress Casual A-Line
               Dresses, Long Sleeve Dresses
             </Column>
