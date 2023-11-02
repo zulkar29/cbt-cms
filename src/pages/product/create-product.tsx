11e033870473bc9a2828e8d7287414ecae424956
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-11-02 12:22:00 +0600

    local commit

--- src/pages/product/create-product.tsx
+++ src/pages/product/create-product.tsx
@@ -47,10 +47,6 @@ const CreateProduct: React.FC = () => {
     setCampaignDate(dateRange);
   };
 
-  const handleDateRangeChange = (dateRange: [Date, Date]) => {
-    setCampaignDate(dateRange);
-  };
-
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       const file = e.target.files[0];
