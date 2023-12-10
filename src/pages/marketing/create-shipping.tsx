4d25d08e08bc7c8c92ae852e1b90e01b384ffed0
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-12-10 23:51:45 +0600

    shipping ui update

--- src/pages/marketing/create-shipping.tsx
+++ src/pages/marketing/create-shipping.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import CardBody from '../../components/card-body';
+import Display from '../../components/display';
+import Input from '../../components/forms/text-input';
+import { Button } from '../../components/button';
 
 const CreateShipping = () => {
-  return <div>CreateShipping</div>;
+  return (
+    <div>
+      <CardBody header="Create Shipping" to="/shipping" text="Back" />
+
+      <Display>
+        <form>
+          <Input label="District Name" htmlFor="name" />
+          <Input label="Price" htmlFor="name" />
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Display>
+    </div>
+  );
 };
 
 export default CreateShipping;
